a87140d6163a6554edef648407b25fc2d89f5041
kartikrana07 <kartikrana8284@gmail.com>
2025-07-29 17:24:17 +0530

    Add files via upload

---
+++ bs2.py
@@ -0,0 +1,59 @@
+
+                                                                        #list[]
+
+#  mylist=[ "apple","banana","orange" ,"grapes"]
+# mylist.remove("apple")
+# mylist.append("guava")
+# print( mylist)
+                                                                      #tupple()
+# mytupple=("apple", "banana","grapes" ,34,)
+# print(mytupple)
+                                                                     #dictionaries
+# mydict=
+# {
+# "name":"kartik",
+# "branch": "cse",
+# "section":"b"
+# }
+# del mydict["name"]       #for delete any key value              #loop in d
+# del mydict["branch"]
+# for i in mydict:                    
+#     print(i,"=",mydict[i])
+
+# del mydict["city"]           # Removes 'city'
+# mydict.pop("age")            # Also removes 'age'
+# mydict.clear()               # Clears all items
+
+
+#  mydict.keys()      # Returns all keys
+# mydict.values()    # Returns all values
+# mydict.items()     # Returns all key-value pairs
+
+                                                                #code for time and date 
+# import datetime
+# a= datetime.datetime.today()
+# print("result=",a)
+                                       
+
+# import datetime as r                                     # r is a shortcut for datetime 
+# a= r.datetime.today()
+# print("result=",a)
+#                                                          #f string
+#                                      
+#  tip:use double braces{{dhuf}} to print as it is in f string
+#print ("hey i am kartik and mutiplication of two numbers is=" f"{23*34}" ,"thats the answer")     
+# 
+#                
+
+                                                            #doc string 
+#  def add(a,b):
+    
+#     #always written after defining variables or paramters
+#     '''     
+#     this is a addition pogramme .There are two parameters a and b
+#     '''
+#     return a+b
+# print(add(5,6))
+# print(add.__doc__)
+
+                    
