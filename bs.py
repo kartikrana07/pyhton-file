a9d70252ca601b615387cfda108ed0e4a1957221
kartikrana07 <kartikrana8284@gmail.com>
2025-07-29 17:21:55 +0530

    Add files via upload

---
+++ bs.py
@@ -0,0 +1,96 @@
+# # print("hello kartik ")
+# # a=input("enter a=")
+# # b= (input("enter b"))
+# # print(a+ b)
+                                                                      #space in string 
+# # print (''' hello everyone my nmae is kartik rana " 
+# # "so if i didn't not "win" what i will do ''')
+
+                                                                       # string slicing
+# # a=("kartik")
+# # print(a[0])
+# # for character in a:
+# #  print(character)
+
+
+# # name = "kartik"
+# # print(name[-4:-2])
+
+                                                                         # string methods 
+# # content=" hello everyone i am Kartik rana a python developer  "
+# # print(content.capitalize())
+
+                                                                          # if elif statements 
+# a=int(input("enter a:"))
+# b=int(input("enter b:"))
+# c=int(input("enter c:"))
+# if a>b and a>c:
+#         print("a is greater")
+# if a==b:
+#       print("a is equals to b")
+# elif b>c:
+#        print(" b is greater ") 
+# else :
+#     print(" c is greater ")                 
+                                                                           #match case
+# a=int(input("enter a:"))
+# b=int (input("enter b:"))
+# operator=input("enter operator (+,-,*,/)::")
+
+# match operator:
+#     case"+":
+#         print("result=", a+b)
+#     case"-":
+#         print("result=", a-b)
+
+#     case"*":
+#         print("result=", a*b)
+#     case"/":
+#         print("result=", a/b)
+#     # default case _
+#     case _:  
+# #         print("can't recoganize operator")
+                                                                         #for loop 
+# for i in range(1,100,2):                                      
+#     print(i)
+# for i in range(1,101):                                               
+#     if i in (12,45,56):   #tupple 
+#               continue     
+#     print(i)
+                                                                           # while loop                                      
+# i=1
+# while(i<=20):
+#     if i==10 :
+#      i=i+1
+#      continue
+#     print(i)
+#     i=i+1
+                                                                         #def function                                  
+# def addition(a,b):
+#   return a+b
+
+
+# def substraction(a,b):
+#   return a-b
+
+# def multiplication(a,b,):
+#   return a*b
+
+
+# num1=int(input("enter num1="))
+# num2=int(input("enter num2="))
+# c=addition(num1,num2)
+# print("result=",c )
+
+# num1=int(input("enter num1="))
+# num2=int(input("enter num2="))
+# c=multiplication(num1,num2)
+# print("result=",c )
+
+# num3=int(input("enter num3="))
+# num4=int(input("enter num4="))
+# c=substraction (num3,num4)
+# print("result=",c)
+
+
+    
